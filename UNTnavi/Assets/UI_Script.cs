2cb29e6ddf75eee74d72c83d30a1559585d2c367
reesehyatt <reesehyatt@gmail.com>
2023-04-04 17:39:56 -0500

    UI Changing Script
    Wrote a script to swap between the main menu and the room search screens. Room Search in progress.

---
+++ UNTnavi/Assets/UI_Script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Script : MonoBehaviour
+{
+    public GameObject mainMenuObj;
+    public GameObject roomSearchObj;
+    public int screenState = 0;
+
+    void Start()
+    {
+        mainMenuObj.SetActive(true);
+        roomSearchObj.SetActive(false);
+    }
+
+    public void update(int state)
+    {
+        screenState = state;
+        
+        if (screenState == 0)
+        {
+            mainMenuObj.SetActive(true);
+            roomSearchObj.SetActive(false);
+        }
+        else if (screenState == 1)
+        {
+            mainMenuObj.SetActive(false);
+            roomSearchObj.SetActive(true);
+        }
+    }
+}
